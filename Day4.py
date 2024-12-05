23480c44353c291645a6a9893b35824919c216e7
narrativityy <jake_crowley@comcast.net>
2024-12-04 20:23:05 -0800

    removed print statements

--- Day4.py
+++ Day4.py
@@ -10,22 +10,18 @@ def calculate_total(txt):
     for i in range(len(txt)):
         for j in range(len(txt[i])):
             if j - len(txt[i]) < 0 and (txt[i][j: j + 4] == 'XMAS' or reverse_string(txt[i][j: j + 4]) == 'XMAS'):
-                # print(reverse_string(txt[i][j:j + 4]))
                 total += 1
             if len(txt) > i + 3 and j < len(txt[i+1]) and j < len(txt[i+2]) and j < len(txt[i+3]):
                 temp_txt = f'{txt[i][j]}{txt[i+1][j]}{txt[i+2][j]}{txt[i+3][j]}'
                 if temp_txt == 'XMAS' or reverse_string(temp_txt) == 'XMAS':
-                    print(temp_txt)
                     total += 1
             if len(txt) > i + 3 and j + 1 < len(txt[i+1]) and j + 2 < len(txt[i+2]) and j + 3 < len(txt[i+3]):
                 temp_txt = f'{txt[i][j]}{txt[i+1][j+1]}{txt[i+2][j+2]}{txt[i+3][j+3]}'
                 if temp_txt == 'XMAS' or reverse_string(temp_txt) == 'XMAS':
-                    print(temp_txt)
                     total += 1
             if len(txt) > i + 3 and j - 1 >= 0 and j - 2 >= 0 and j - 3 >= 0:
                 temp_txt = f'{txt[i][j]}{txt[i+1][j-1]}{txt[i+2][j-2]}{txt[i+3][j-3]}'
                 if temp_txt == 'XMAS' or reverse_string(temp_txt) == 'XMAS':
-                    print(temp_txt)
                     total += 1
 
     
